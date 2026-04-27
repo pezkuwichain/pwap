09da6e80b7f86bafc10f7ee27890fa7d590996da
Kurdistan Tech Ministry <info@pezkuwichain.io>
2026-04-27 03:06:15 +0300

    chore: remove mobile/ from monorepo, suspend CI mobile job
    Mobile app moved to /home/mamostehp/pwap-mobile (local, suspended).
    Will be re-integrated when mobile development resumes.
    
    - Removed mobile/ directory entirely
    - Removed Mobile App job from quality-gate.yml so CI no longer blocks

--- mobile/src/components/icons/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as HezTokenLogo } from './HezTokenLogo';
-export { default as PezTokenLogo } from './PezTokenLogo';
--- mobile/src/theme/colors.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Re-export colors from shared theme
- * All color definitions are centralized in shared/theme/colors.ts
- */
-export { KurdistanColors, AppColors } from '../../../shared/theme/colors';
-export { AppColors as default } from '../../../shared/theme/colors';
